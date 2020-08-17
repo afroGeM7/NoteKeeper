cfa60288ff53d256cfb06f97519b791ef5100bdf
Gemma Usiku <afrogem3@gmail.com>
2020-08-18 02:47:20 +0800

    initial commit

---
+++ app/src/test/java/com/gemmausiku/notekeeper/DataManagerTest.java
@@ -0,0 +1,14 @@
+package com.gemmausiku.notekeeper;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DataManagerTest {
+
+    @Test
+    public void createNewNote() {
+        final CourseInfo course = DataManager.getInstance().getCourse("android_sync");
+        
+    }
+}
