25ebf9acef1b63c495f2fee61c1ee00e410cc1f8
Gemma Usiku <afrogem3@gmail.com>
2020-08-18 03:29:43 +0800

    initial commit

--- app/src/test/java/com/gemmausiku/notekeeper/DataManagerTest.java
+++ app/src/test/java/com/gemmausiku/notekeeper/DataManagerTest.java
@@ -8,7 +8,23 @@ public class DataManagerTest {
 
     @Test
     public void createNewNote() {
-        final CourseInfo course = DataManager.getInstance().getCourse("android_sync");
-        
+        DataManager dm = DataManager.getInstance();
+        final CourseInfo course = dm.getCourse("android_sync");
+        final String noteTitle = "Test note title";
+        final String noteText = "This is the body of the text of my test note";
+
+        int noteIndex = dm.createNewNote();
+        NoteInfo newNote = dm.getNotes().get(noteIndex);
+        newNote.setCourse(course);
+        newNote.setTitle(noteTitle);
+        newNote.setText(noteText);
+
+        NoteInfo compareNote = dm.getNotes().get(noteIndex);
+        assertEquals(compareNote.getCourse(), course);
+        assertEquals(compareNote.getTitle(), noteTitle);
+        assertEquals(compareNote.getText(), noteText);
+
+
+
     }
 }
