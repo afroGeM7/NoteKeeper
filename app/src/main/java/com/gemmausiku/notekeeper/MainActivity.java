31c352afc2c7ccf8d3586e844cdc6ca032f202bb
Gemma Usiku <afrogem3@gmail.com>
2020-08-30 02:46:54 +0800

    initial commit

--- app/src/main/java/com/gemmausiku/notekeeper/MainActivity.java
+++ app/src/main/java/com/gemmausiku/notekeeper/MainActivity.java
@@ -77,7 +77,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private void initializeDisplayContent() {
         mRecyclerItems = (RecyclerView) findViewById(R.id.list_items);
         mNotesLayoutManager = new LinearLayoutManager(this);
-        mCoursesLayoutManager = new GridLayoutManager(this, 2);
+        mCoursesLayoutManager = new GridLayoutManager(this,
+                getResources().getInteger(R.integer.course_grid_span));
 
         List<NoteInfo> notes = DataManager.getInstance().getNotes();
         mNoteRecyclerAdapter = new NoteRecyclerAdapter(this, notes);
@@ -154,9 +155,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         } else if (id == R.id.nav_courses) {
             displayCourses();
         } else if (id == R.id.nav_share) {
-            handleSelection("Don't you think you've shared enough");
+            handleSelection(R.string.nav_share_message);
         } else if (id == R.id.nav_send) {
-            handleSelection("Send");
+            handleSelection(R.string.nav_send_message);
         }
 
 
@@ -165,9 +166,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return true;
 
 }
-    private void handleSelection(String message) {
+    private void handleSelection(int message_id) {
         View view = findViewById(R.id.list_items);
-        Snackbar.make(view,message, Snackbar.LENGTH_LONG).show();
+        Snackbar.make(view,message_id, Snackbar.LENGTH_LONG).show();
 
     }
 
